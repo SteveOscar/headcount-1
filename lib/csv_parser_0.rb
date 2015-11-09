a4cf0e3cba471377bae29ae57b808a2472e3288d
Steven Olson <steveoscaro@gmail.com>
2015-11-09 16:27:01 -0700

    first commit, working on parser class and test

---
+++ lib/csv_parser_0.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'csv'
+
+class CSVParser
+  attr_accessor :csv_data
+
+  def load_data(path)
+    @csv_data = CSV.open(path, {:headers => true})
+  end
+
+  def get_districts
+    districts = @csv_data.readlines.map { |line| line[0] }
+    districts.uniq!
+  end
+
+
+
+
+end

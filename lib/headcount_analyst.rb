b7fadff650dedd9c88bcce0bd271fad63e5fcda5
Steven Olson <steveoscaro@gmail.com>
2015-11-11 19:05:53 -0700

    refactored headcount analyst

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -8,9 +8,7 @@ class HeadcountAnalyst
   def kindergarten_participation_rate_variation(district, compared_to)
     initial = dr.find_by_name(district)
     comparison = dr.find_by_name(compared_to[:against])
-    num = average_values(initial.enrollment.enrollment_data[:kindergarten])
-    dem = average_values(comparison.enrollment.enrollment_data[:kindergarten])
-    (num / dem).round(3)
+    find_variation(initial, comparison, :kindergarten)
   end
 
   def average_values(array)
@@ -18,13 +16,17 @@ class HeadcountAnalyst
     answer = array.reduce(:+) / array.length
   end
 
+  def find_variation(initial, comparison, grade)
+    num = average_values(initial.enrollment.enrollment_data[grade])
+    dem = average_values(comparison.enrollment.enrollment_data[grade])
+    (num / dem).round(3)
+  end
+
   def kindergarten_participation_rate_variation_trend(district, compared_to)
     answers = {}
     initial = dr.find_by_name(district).enrollment.enrollment_data.values.first
     comparison = dr.find_by_name(compared_to.values.join).enrollment.enrollment_data.values.first
-    initial.each do |k, v|
-      answers[k] = (v.to_f / comparison[k].to_f).round(3)
-    end
+    initial.each { |k, v| answers[k] = (v.to_f / comparison[k].to_f).round(3) }
     answers
   end
 
@@ -32,10 +34,7 @@ class HeadcountAnalyst
     kindergarten_variation = kindergarten_participation_rate_variation(district, :against => "COLORADO")
     initial = dr.find_by_name(district)
     comparison = dr.find_by_name("COLORADO")
-    num = average_values(initial.enrollment.enrollment_data[:high_school_graduation])
-    dem = average_values(comparison.enrollment.enrollment_data[:high_school_graduation])
-    graduation_variation = (num / dem).round(3)
-    kindergarten_graduation_variance = (kindergarten_variation / graduation_variation).round(3)
+    (kindergarten_variation / (find_variation(initial, comparison, :high_school_graduation))).round(3)
   end
 
 

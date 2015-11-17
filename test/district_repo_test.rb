f33976616c466c9a1a11e0c2db43c775b17ecc8b
Steven Olson <steveoscaro@gmail.com>
2015-11-17 08:46:51 -0700

    added full data to district repo test and fixed tests

--- test/district_repo_test.rb
+++ test/district_repo_test.rb
@@ -9,7 +9,7 @@ class DistrictRepositoryTest < Minitest::Test
 
   def setup
     @dr = DistrictRepository.new
-    dr.load_data({:enrollment => {:kindergarten => "./test/fixtures/sample_kindergarten_data.csv", :high_school_graduation => "./data/High school graduation rates.csv"},
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv", :high_school_graduation => "./data/High school graduation rates.csv"},
                 :economic_profile => {
                           :median_household_income => "./data/Median household income.csv",
                           :children_in_poverty => "./data/School-aged children in poverty.csv",
@@ -30,11 +30,11 @@ class DistrictRepositoryTest < Minitest::Test
 
   def test_can_load_data_from_given_file
     assert_equal "COLORADO", dr.districts[0].name
-    assert_equal "HI-PLAINS R-23", dr.districts[4].name
+    assert_equal "AGATE 300", dr.districts[4].name
   end
 
   def test_creates_correct_number_of_objects_in_array
-    assert_equal 8, dr.districts.length
+    assert_equal 181, dr.districts.length
   end
 
   def test_find_by_name_finds_object
@@ -64,15 +64,15 @@ class DistrictRepositoryTest < Minitest::Test
   end
 
   def test_find_all_matching_names_finds_objects
-    object = dr.find_all_matching("ADAMS COUNTY")
+    object = dr.find_all_matching("ADAMS")
     assert_equal "ADAMS COUNTY 14", object[0].name
-    assert_equal "ADAMS COUNTY 15", object[1].name
+    assert_equal "ADAMS-ARAPAHOE 28J", object[1].name
   end
 
   def test_find_all_matching_names_finds_objects_with_lowercase
-    object = dr.find_all_matching("adams county")
+    object = dr.find_all_matching("adams")
     assert_equal "ADAMS COUNTY 14", object[0].name
-    assert_equal "ADAMS COUNTY 15", object[1].name
+    assert_equal "ADAMS-ARAPAHOE 28J", object[1].name
   end
 
   def test_can_link_district_objects_to_newly_created_enrollment_objects
@@ -91,35 +91,22 @@ class DistrictRepositoryTest < Minitest::Test
   end
 
   def test_can_handle_full_dataset_and_create_necessary_objects
-    dr.load_data({:enrollment => {:kindergarten => "./data/kindergartners in full-day program.csv"}})
-    district = dr.find_by_name("ACADEMY 20")
-    assert_equal "ACADEMY 20", district.enrollment.district
-
     answer = {2007=>0.39465, 2006=>0.33677, 2005=>0.27807, 2004=>0.24014, 2008=>0.5357, 2009=>0.598, 2010=>0.64019, 2011=>0.672, 2012=>0.695, 2013=>0.70263, 2014=>0.74118}
     district = dr.find_by_name("COLORADO")
     assert_equal answer, district.enrollment.enrollment_data[:kindergarten]
   end
 
   def test_can_load_high_school_data
-    dr.load_data({:enrollment => {:high_school_graduation => "./test/fixtures/sample_hs_graduation_data.csv"}})
     district = dr.find_by_name("ACADEMY 20")
     assert_equal "ACADEMY 20", district.enrollment.district
   end
 
   def test_can_load_two_data_sets_of_different_classes
-    dr.load_data({:enrollment => {:high_school_graduation => "./test/fixtures/sample_hs_graduation_data.csv"}, :statewide_testing => {:third_grade => "./test/fixtures/sample_3rd_grade_data.csv"}})
     district = dr.find_by_name("ACADEMY 20")
     assert_equal "ACADEMY 20", district.enrollment.district
   end
 
   def test_can_load_all_releveant_data_files_intergration_test
-    dr.load_data({:enrollment => {:high_school_graduation => "./data/High school graduation rates.csv", :kindergarten => "./data/kindergartners in full-day program.csv"},
-    :statewide_testing => {
-    :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-    :eigth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-    :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-    :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-    :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"}})
     assert_equal 181, dr.districts.count
     assert_equal "YUMA SCHOOL DISTRICT 1", dr.districts.last.name
   end

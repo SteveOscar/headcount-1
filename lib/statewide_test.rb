2714b0b9c3d45a2e306a67693dbc67ac146dd27b
Steven Olson <steveoscaro@gmail.com>
2015-11-18 08:45:46 -0700

    refactored statewide_test object class

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -9,16 +9,8 @@ class StatewideTest
     @test_data = {data.keys[1] => data.values[1]}
   end
 
-  def years
-    [2008, 2009, 2010, 2011, 2012, 2013, 2014]
-  end
-
   def races
     [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
-  end
-
-  def subjects
-    [:math, :reading, :writing]
   end
 
   def truncate(num)
@@ -38,47 +30,41 @@ class StatewideTest
 
   def proficient_by_grade(grade) ## we tested with 'COLORADO', but we need to specificy COLORADO?
     truncate_hash_values(test_data)
+    raise UnknownDataError.new if grade != 3 && grade != 8
     if grade == 3
       test_data[:third_grade]
-    elsif grade == 8
-      test_data[:eigth_grade]
     else
-      raise UnknownDataError.new
+      test_data[:eigth_grade]
     end
   end
 
   def proficient_by_race_or_ethnicity(race)
-    if !races.include?(race)
-      raise UnknownRaceError.new
-    else
-      data = {}
-      test_data[:math].each { |key| data.merge!({ key[0] => {:math => truncate(key[1][race]) }})}
-      test_data[:reading].each { |key| data[key[0]].merge!({:reading => truncate(key[1][race]) }) }
-      test_data[:writing].each { |key| data[key[0]].merge!({:writing => truncate(key[1][race]) }) }
-    end
+    raise UnknownRaceError.new if !races.include?(race)
+    data = {}
+    test_data[:math].each { |key| data.merge!({ key[0] => {:math => truncate(key[1][race]) }})}
+    test_data[:reading].each { |key| data[key[0]].merge!({:reading => truncate(key[1][race]) }) }
+    test_data[:writing].each { |key| data[key[0]].merge!({:writing => truncate(key[1][race]) }) }
     data
   end
 
   def proficient_for_subject_by_grade_in_year(subject, grade, year)
-    raise UnknownDataError.new if !subjects.include?(subject)
-    raise UnknownDataError.new if !years.include?(year)
-    raise UnknownDataError.new if grade != 3 && grade != 8
-    case grade
-    when 3
-      grade = :third_grade
-    when 8
-      grade = :eigth_grade
-    else
-      raise UnknownDataError.new
-    end
-    truncate((test_data[grade][year][subject]).to_f)
+    raise UnknownDataError.new unless [:math, :reading, :writing].include?(subject)
+    raise UnknownDataError.new unless (2008..2014).include?(year)
+    raise UnknownDataError.new unless grade == 3 || grade == 8
+    grade == 3 ? grade = :third_grade : grade = :eigth_grade
+    format_profeiciency_result(subject, grade, year)
   end
 
   def proficient_for_subject_by_race_in_year(subject, race, year)
-    raise UnknownDataError.new if !subjects.include?(subject)
-    raise UnknownDataError.new if !years.include?(year)
-    raise UnknownDataError.new if !races.include?(race)
+    raise UnknownDataError.new unless [:math, :reading, :writing].include?(subject)
+    raise UnknownDataError.new unless (2008..2014).include?(year)
+    raise UnknownDataError.new unless races.include?(race)
     truncate((test_data[subject][year][race]).to_f)
+  end
+
+  def format_profeiciency_result(subject, grade, year)
+    answer = truncate((test_data[grade][year][subject]).to_f)
+    answer == 0.0 ? "N/A" : answer
   end
 
 end
